1d0f01e8d69f5c710719eb1d7dab0ae02448b6cf
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-07-14 22:04:46 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
-  has_many :categories, through: :post_categories
-  has_many :comments
-  has_many :users, through: :comments
+ has_many :categories, through: :post_categories
+ has_many :comments
+ has_many :users, through: :comments
 
+ def comment_count
+   comments.length
+ end
 
+ def categories_attributes= category_attrs
+   category_attrs.values.each do |cat_attr|
+     self.categories << Category.find_or_create_by(cat_attr) unless cat_attr.values[0].empty?
+   end
+ end
 end
